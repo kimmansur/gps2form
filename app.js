d3d4f295f56e6684aef04eaf2fb27d08f7d9b0e9
Kim Mansur Yano <kimmansur@gmail.com>
2023-07-14 14:23:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 // Custom Functions
 
-// Definindo as datas permitidas
-var allowedDates = ["2023-07-07", "2023-07-14", "2023-07-14", "2023-08-03", "2023-09-01", "2023-10-06", "2023-11-03", "2023-12-01"];
-// Definindo o horário de inicio e término
-var startTime = 13.83; // 13:50
-var endTime = 19; // 19:00
+// // Definindo as datas permitidas
+// var allowedDates = ["2023-07-07", "2023-07-14", "2023-07-14", "2023-08-03", "2023-09-01", "2023-10-06", "2023-11-03", "2023-12-01"];
+// // Definindo o horário de inicio e término
+// var startTime = 13.83; // 13:50
+// var endTime = 19; // 19:00
 
-// Definindo a diferença de fuso horário
-var timezoneOffset = -240; // para GMT-0400
+// // Definindo a diferença de fuso horário
+// var timezoneOffset = -240; // para GMT-0400
 
-function checkDate() {
-    var now = new Date();
-    var timezoneDifference = timezoneOffset - now.getTimezoneOffset();
-    now.setMinutes(now.getMinutes() + timezoneDifference); // ajusta a hora local para GMT-0400
+// function checkDate() {
+//     var now = new Date();
+//     var timezoneDifference = timezoneOffset - now.getTimezoneOffset();
+//     now.setMinutes(now.getMinutes() + timezoneDifference); // ajusta a hora local para GMT-0400
 
-    var currentDate = now.toISOString().slice(0,10); // pega a data atual no formato yyyy-mm-dd
-    var currentTime = now.getHours() + now.getMinutes() / 60; // pega a hora atual e converte em decimal
+//     var currentDate = now.toISOString().slice(0,10); // pega a data atual no formato yyyy-mm-dd
+//     var currentTime = now.getHours() + now.getMinutes() / 60; // pega a hora atual e converte em decimal
 
-    // checando se a data e hora atual estão dentro do intervalo permitido
-    if (allowedDates.includes(currentDate) && currentTime >= startTime && currentTime <= endTime) {
-        return true;
-    } else {
-        return false;
-    }
-}
+//     // checando se a data e hora atual estão dentro do intervalo permitido
+//     if (allowedDates.includes(currentDate) && currentTime >= startTime && currentTime <= endTime) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
 // Função para encontrar a próxima data permitida
-function getNextDate() {
-    var now = new Date();
-    var nextDate = null;
+// function getNextDate() {
+//     var now = new Date();
+//     var nextDate = null;
 
-    for (var i = 0; i < allowedDates.length; i++) {
-        var currentDate = new Date(allowedDates[i]);
-        if (currentDate > now) {
-            nextDate = currentDate;
-            break;
-        }
-    }
+//     for (var i = 0; i < allowedDates.length; i++) {
+//         var currentDate = new Date(allowedDates[i]);
+//         if (currentDate > now) {
+//             nextDate = currentDate;
+//             break;
+//         }
+//     }
 
-    return nextDate;
-}
+//     return nextDate;
+// }
 
 function setNote() {
     const queryString = new URLSearchParams(window.location.search);
@@ -50,14 +50,14 @@ function setNote() {
 }
 
 window.onload = function() {
-    if (!checkDate()) {
-        var nextDate = getNextDate();
-        if (nextDate) {
-            // Formata a data em Dia/Mês/Ano
-            var day = nextDate.getDate();
-            var month = nextDate.getMonth() + 1; // o índice do mês começa em 0
-            var year = nextDate.getFullYear();
-            var formattedDate = day + "/" + month + "/" + year;
+    // if (!checkDate()) {
+    //     var nextDate = getNextDate();
+    //     if (nextDate) {
+    //         // Formata a data em Dia/Mês/Ano
+    //         var day = nextDate.getDate();
+    //         var month = nextDate.getMonth() + 1; // o índice do mês começa em 0
+    //         var year = nextDate.getFullYear();
+    //         var formattedDate = day + "/" + month + "/" + year;
 
         //     // document.getElementById("app").innerHTML = "<div id='customAlert' class='custom-alert'>" +
         //     "<div class='alert alert-error'>" + 
